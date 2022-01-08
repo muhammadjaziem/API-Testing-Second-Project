bd94657aa0660c43d51838ee6f2966a40f3d7243
Muhammad Jaziem <muhammadjaziem21@gmail.com>
2022-01-09 00:27:18 +0800

    latest commit

---
+++ src/test/java/utilities/RestUtils.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class RestUtils {
+	
+	public static String empName()
+	{
+		String generatedString =RandomStringUtils.randomAlphabetic(1);
+		return ("John" + generatedString);
+	}
+	
+	public static String empSal()
+	{
+		String generatedString =RandomStringUtils.randomNumeric(5);
+		return (generatedString);
+	}
+	
+	public static String empAge()
+	{
+		String generatedString =RandomStringUtils.randomNumeric(5);
+		return (generatedString);
+	}
+	
+	public static String empJob()
+	{
+		String generatedString ="Lawyer";
+		return (generatedString);
+	}
+
+}
